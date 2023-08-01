d1b5bb65579471e15bb29d0f6655877219e2fc1f
Otabek <mavlonovo@tamu.edu>
2023-08-01 14:12:13 -0500

    added write file log

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ inquirer
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_image.png"));
 
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+
     // console.log(answers);
   })
   .catch((error) => {
@@ -29,5 +34,4 @@ inquirer
 
 /*2. Use the qr-image npm package to turn the user entered URL into a QR code image.*/
 
-/*3. Create a txt file to save the user input using the native fs node module.
- */
+/*3. Create a txt file to save the user input using the native fs node module.*/
